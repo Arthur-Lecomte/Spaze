484bc5c93ea4d45d973dcea6d57f480c71e8a9fa
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-03-13 00:01:29 +0100

    {Update} Drag & Drop Inventory
    Lorsque l'on possède des Constructions dans l'inventaire, permet de les déplacer d'un slot à un autre avec la souris

--- Assets/Scripts/Player/Inventory/Inventory.cs
+++ Assets/Scripts/Player/Inventory/Inventory.cs
@@ -2,29 +2,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour {
+    public static Inventory Instance; //DEBUG!!! Pour des tests rapides. À enlever et corriger plus tard
+
     private Dictionary<TypeRessource, int> ressources;
     private List<VaisseauModule> composants;
     private List<Construction> constructionsInventory;
     [SerializeField] private int size = 2;
+    [SerializeField] private Transform parentInInventory;
+
+    public GameObject prefabTest;
+    public GameObject prefabTest2;
 
     private void Awake() {
+        if(Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+
         ressources = new Dictionary<TypeRessource, int>();
         foreach(TypeRessource type in System.Enum.GetValues(typeof(TypeRessource))) {
             ressources.Add(type, 0);
         }
-        
+
         composants = new List<VaisseauModule>(GetComponentsInChildren<VaisseauModule>(true));
         constructionsInventory = new List<Construction>(size);
+        for(int i = 0; i < size; i++) {
+            constructionsInventory.Add(null);
+        }
     }
 
     private void Start() {
         InventoryUI.Instance.ChangeNumberSlots(size);
+        GameObject go = Instantiate(prefabTest);
+        AddConstruction(go.GetComponent<Construction>());
+        
+        go = Instantiate(prefabTest2);
+        AddConstruction(go.GetComponent<Construction>());
     }
 
     public void AddRessource(TypeRessource type, int quantity) {
         ressources[type] += quantity;
     }
-    
+
+    public bool HaveEnoughRessources(Dictionary<TypeRessource, int> allRessources) {
+        foreach((TypeRessource type, int quantity) in allRessources) {
+            if(!HaveEnoughRessource(type, quantity)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public bool HaveEnoughRessource(TypeRessource type, int quantity) {
         return ressources[type] >= quantity;
     }
@@ -33,37 +62,44 @@ public class Inventory : MonoBehaviour {
     public void RemoveRessource(TypeRessource type, int quantity) {
         ressources[type] -= quantity;
     }
-    
+
     public int GetRessource(TypeRessource type) {
         return ressources[type];
     }
-    
+
     public void AddInventorySlots() {
         size += 1;
         constructionsInventory.Add(null);
         InventoryUI.Instance.ChangeNumberSlots(size);
     }
 
+    public void MoveInventorySlot(int index, int newIndex) {
+        (constructionsInventory[index], constructionsInventory[newIndex]) = (constructionsInventory[newIndex], constructionsInventory[index]);
+        InventoryUI.Instance.UpdateSlotImage(index, constructionsInventory[index]);
+        InventoryUI.Instance.UpdateSlotImage(newIndex, constructionsInventory[newIndex]);
+    }
+
     // Méthode pour ajouter une construction au vaisseau
     public bool AddConstruction(Construction newConstruction) {
         VaisseauModule libre = null;
-        
+
         // On vérifie si on peut améliorer une construction posée sur le vaisseau
         foreach(VaisseauModule composant in composants) {
-            if(composant.GetConstruction().IsSameConstruction(newConstruction)) {
+            Construction construction = composant.GetConstruction();
+            if(construction && construction.IsSameConstruction(newConstruction)) {
                 composant.GetConstruction().Upgrade();
                 return true;
             }
-            
+
             // On garde en mémoire un composant libre au cas où on ne peut rien améliorer
             if(!libre && !composant.IsEmpty()) {
                 libre = composant;
             }
         }
-        
+
         // On vérifie si on peut améliorer une construction présente dans l'inventaire
         foreach(Construction construction in constructionsInventory) {
-            if(construction.IsSameConstruction(construction)) {
+            if(construction && construction.IsSameConstruction(construction)) {
                 construction.Upgrade();
                 return true;
             }
@@ -74,15 +110,17 @@ public class Inventory : MonoBehaviour {
             libre.SetConstruction(newConstruction);
             return true;
         }
-        
+
         // Si on a de la place dans l'inventaire, on ajoute la construction
-        for (int i = 0; i < constructionsInventory.Count; i++) {
-            if (!constructionsInventory[i]) {
+        for(int i = 0; i < constructionsInventory.Count; i++) {
+            if(!constructionsInventory[i]) {
                 constructionsInventory[i] = newConstruction;
+                newConstruction.transform.SetParent(parentInInventory);
+                InventoryUI.Instance.UpdateSlotImage(i, newConstruction);
                 return true;
             }
         }
-        
+
         // On ne peut rien faire de cette construction, on annule l'achat
         return false;
     }
--- Assets/Scripts/Player/Inventory/InventoryUI.cs
+++ Assets/Scripts/Player/Inventory/InventoryUI.cs
@@ -1,34 +1,111 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour {
     public static InventoryUI Instance;
 
     [SerializeField] private GameObject slotPrefab;
+    [SerializeField] private GameObject dragImagePrefab;
     private RectTransform rectTransform;
+
+    private List<GameObject> constructionsList;
     private int numberSlotsMemory;
-    
+
+    private GameObject dragImage;
+    private Image dragImageComponent;
+    private int draggedSlotIndex = -1;
+
     private void Awake() {
         if(Instance == null) {
             Instance = this;
         } else {
             Destroy(gameObject);
         }
-        
+
         rectTransform = GetComponent<RectTransform>();
         numberSlotsMemory = 0;
+        constructionsList = new List<GameObject>();
+
+        dragImage = Instantiate(dragImagePrefab, transform);
+        dragImageComponent = dragImage.GetComponent<Image>();
+        dragImage.SetActive(false);
     }
-    
+
     public void ChangeNumberSlots(int numberSlots) {
         int columns = (numberSlots + 1) / 2;
         rectTransform.sizeDelta = new Vector2(15 + 105 * columns, 250);
         rectTransform.anchoredPosition = new Vector2(rectTransform.sizeDelta.x / 2 + 25, 150);
-        
-        for (int i = numberSlotsMemory + 1; i < numberSlots + 1; i++) {
+
+        for(int i = numberSlotsMemory + 1; i < numberSlots + 1; i++) {
             int column = (i + 1) / 2;
             GameObject slot = Instantiate(slotPrefab, rectTransform);
+            constructionsList.Add(slot);
             RectTransform slotRectTransform = slot.GetComponent<RectTransform>();
             slotRectTransform.anchoredPosition = new Vector3(-45 + 105 * column, -65 + 105 * (i % 2), 0);
+
+            EventTrigger trigger = slot.AddComponent<EventTrigger>();
+            int index = i - 1;
+            AddEventTrigger(trigger, EventTriggerType.PointerDown, (data) => OnPointerDown((PointerEventData)data, slot, index));
+            AddEventTrigger(trigger, EventTriggerType.Drag, (data) => OnDrag((PointerEventData)data));
+            AddEventTrigger(trigger, EventTriggerType.PointerUp, (data) => OnPointerUp((PointerEventData)data));
         }
         numberSlotsMemory = numberSlots;
+    }
+
+    private void AddEventTrigger(EventTrigger trigger, EventTriggerType eventType, System.Action<BaseEventData> action) {
+        EventTrigger.Entry entry = new EventTrigger.Entry { eventID = eventType };
+        entry.callback.AddListener((data) => action(data));
+        trigger.triggers.Add(entry);
+    }
+
+    public void UpdateSlotImage(int index, Construction newConstruction) {
+        Image imageComponent = constructionsList[index].transform.Find("ImageConstruction").GetComponent<Image>();
+        if (newConstruction) {
+            imageComponent.sprite = newConstruction.GetImage();
+        }
+        imageComponent.enabled = newConstruction;
+    }
+
+    private void OnPointerDown(PointerEventData eventData, GameObject slot, int index) {
+        draggedSlotIndex = index;
+        Image imageComponent = slot.transform.Find("ImageConstruction").GetComponent<Image>();
+        dragImageComponent.sprite = imageComponent.sprite;
+        dragImageComponent.color = new Color(1, 1, 1, 0.5f);
+        dragImage.transform.position = eventData.position;
+        dragImage.transform.SetAsLastSibling();
+        dragImage.SetActive(true);
+    }
+
+    private void OnDrag(PointerEventData eventData) {
+        if(dragImage.activeSelf) {
+            dragImage.transform.position = eventData.position;
+        }
+    }
+
+    private void OnPointerUp(PointerEventData eventData) {
+        if(dragImage.activeSelf) {
+            dragImage.SetActive(false);
+
+            PointerEventData pointerEventData = new PointerEventData(EventSystem.current) {
+                position = eventData.position
+            };
+            
+            List<RaycastResult> raycastResults = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+
+            foreach(RaycastResult result in raycastResults) {
+                GameObject slot = result.gameObject;
+                if(constructionsList.Contains(slot)) {
+                    int targetIndex = constructionsList.IndexOf(slot);
+                    if(targetIndex != draggedSlotIndex) {
+                        Inventory.Instance.MoveInventorySlot(draggedSlotIndex, targetIndex);
+                    }
+                    break;
+                }
+            }
+        }
+        draggedSlotIndex = -1;
     }
 }
