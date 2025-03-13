b02e1c6e5aaa9b3bd4616bc3fc88700da9cd7616
Arthur Lecomte <arthur.lecomte@etu.univ-amu.fr>
2025-03-13 19:19:26 +0100

    {Update} camera zoom

--- Assets/Scripts/Camera/CameraZoom.cs
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -5,7 +5,7 @@ public class CameraZoom : MonoBehaviour
     [SerializeField] private Camera cam; // La caméra à modifier
     [SerializeField] private float zoomSpeed = 10f; // Vitesse du zoom
     [SerializeField] private float minZoom = 5f;  // Zoom maximum (le plus proche)
-    [SerializeField] private float maxZoom = 50f; // Zoom minimum (le plus loin)
+    public float maxZoom = 50f; // Zoom minimum (le plus loin)
 
     private void Update()
     {
