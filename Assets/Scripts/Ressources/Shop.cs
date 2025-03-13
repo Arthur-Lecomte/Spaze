347bce39f795cafae0a53fd05a095b05f794f7f7
Cariat Baptiste <98692128+Skyblue19@users.noreply.github.com>
2025-03-13 01:51:01 +0100

    {Update} Affichage plusieurs constructions
    -Update de la fonction d'affichage pour afficher 3 constructions dont 1 tourelle assurée et 2 autres aléatoires

--- Assets/Scripts/Ressources/Shop.cs
+++ Assets/Scripts/Ressources/Shop.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,20 +11,49 @@ public class Shop : MonoBehaviour {
     [SerializeField] private GameObject prefabConstructionItem;
 
     private void Start() {
+        // Appeler la fonction pour sélectionner et afficher les constructions
+        SelectionnerEtAfficherConstructions();
+    }
+
+        private void SelectionnerEtAfficherConstructions() {
+        // Filtrer les prefabs pour ne garder que les tourelles
+        GameObject[] tourellePrefabs = prefabsConstructions.Where(prefab => prefab.GetComponent<Turret>() != null).ToArray();
+        // Sélectionner une tourelle aléatoire parmi les tourelles filtrées
+        int randomTourelleIndex = Random.Range(0, tourellePrefabs.Length);
+        GameObject tourellePrefab = tourellePrefabs[randomTourelleIndex];
+
+        // Sélectionner deux autres constructions aléatoires parmi toutes les constructions
+        GameObject[] autresPrefabs = prefabsConstructions.Where(prefab => prefab != tourellePrefab).ToArray();
         
-             // Configurer l'élément UI
-            GameObject constructionItem = Instantiate(prefabConstructionItem, conteneurConstructions);
-            TextMeshProUGUI constructionText = constructionItem.transform.Find("ConstructionText")?.GetComponent<TextMeshProUGUI>();
-            Button acheterButton = constructionItem.transform.Find("AcheterButton")?.GetComponent<Button>();
 
-            int randomIndex = Random.Range(0, prefabsConstructions.Length);
-            GameObject selectedPrefab = prefabsConstructions[randomIndex];
-            Construction construction = selectedPrefab.GetComponent<Construction>();
-            constructionText.text = construction.ToString();
-            acheterButton.onClick.AddListener(() => BuyConstruction(construction));
+        int randomIndex1 = Random.Range(0, autresPrefabs.Length);
+        int randomIndex2;
+        do {
+            randomIndex2 = Random.Range(0, autresPrefabs.Length);
+        } while (randomIndex2 == randomIndex1);
 
+        GameObject selectedPrefab1 = autresPrefabs[randomIndex1];
+        GameObject selectedPrefab2 = autresPrefabs[randomIndex2];
+
+        // Afficher les trois constructions dans l'interface utilisateur
+        AfficherConstruction(tourellePrefab);
+        AfficherConstruction(selectedPrefab1);
+        AfficherConstruction(selectedPrefab2);
+    }
+
+    private void AfficherConstruction(GameObject prefab) {
+        // Instancier le prefab de l'élément UI
+        GameObject constructionItem = Instantiate(prefabConstructionItem, conteneurConstructions);
+
+        // Configurer l'élément UI
+        TextMeshProUGUI constructionText = constructionItem.transform.Find("ConstructionText")?.GetComponent<TextMeshProUGUI>();
+        Button acheterButton = constructionItem.transform.Find("AcheterButton")?.GetComponent<Button>();
+        Construction construction = prefab.GetComponent<Construction>();
+        constructionText.text = construction.ToString();
+        acheterButton.onClick.AddListener(() => BuyConstruction(construction));
         
     }
+
 
     // Méthode pour acheter une construction
     private bool BuyConstruction(Construction construction) {
@@ -45,7 +75,6 @@ public class Shop : MonoBehaviour {
             // Ajouter la construction au vaisseau
             vaisseau.inventory.AddConstruction(construction);
             //TEMPORAIRE
-            Debug.Log("Construction achetée");
             vaisseau.inventory.ShowConstructions();
         }
         return canBuy;
