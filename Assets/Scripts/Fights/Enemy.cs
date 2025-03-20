e85aadc0d65d2691f3209edb56fdcf6a9de71f12
Arthur Lecomte <arthur.lecomte.pro83@gmail.com>
2025-03-20 21:51:33 +0100

    {Fix} double scripte Enemy
    Déplacement des fonctions d'un scripte Enemy à l'autre puis suppression du scripte doublon

--- Assets/Scripts/Fights/Enemy.cs
+++ Assets/Scripts/Fights/Enemy.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
 public class Enemy : MonoBehaviour, ICanTakeDamage {
+
+    public int Level { get; set; } = 1;
+
+    public delegate void EnemyDestroyedHandler(GameObject enemy);
+    public event EnemyDestroyedHandler OnDestroyed;
+
+    private void OnDestroy() {
+        OnDestroyed?.Invoke(gameObject);
+    }
+
     public void TakeDamage(float damage) {
         //TakeDamage
     }
