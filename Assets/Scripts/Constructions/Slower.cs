bb28e40c98ba60dfec0e9e12854d424c268cddc8
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-03-24 22:32:46 +0100

    {Add} Construction Ralentissement
    Les ralentisseurs baissent la vitesse des projectiles ennemis proches du joueur.

--- Assets/Scripts/Constructions/Slower.cs
+++ Assets/Scripts/Constructions/Slower.cs
@@ -3,4 +3,31 @@ using UnityEngine;
 public class Slower : Construction {
     [SerializeField] private float range;
     [SerializeField] private float power;
+    
+    private SphereCollider trigger;
+
+    public override void Initialisation(RarityConstruction rarityConstruction) {
+        base.Initialisation(rarityConstruction);
+
+        trigger = GetComponent<SphereCollider>();
+        trigger.radius = range;
+    }
+    
+    protected override void PerformUpgrade() {
+        trigger.radius = range; //Augmente la portée de l'onde
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Tir projectile = other.GetComponent<Tir>();
+        if (projectile != null && !projectile.IsFromPlayer()) {
+            projectile.InSlowArea(power);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Tir projectile = other.GetComponent<Tir>();
+        if (projectile != null && !projectile.IsFromPlayer()) {
+            projectile.InSlowArea(-power);
+        }
+    }
 }
--- Assets/Scripts/Projectiles/Tir.cs
+++ Assets/Scripts/Projectiles/Tir.cs
@@ -6,6 +6,8 @@ public class Tir : MonoBehaviour {
     private float speed;
     private float range;
     private float distanceTraveled;
+    
+    private float ralentissement = 1;
 
     public void SetInformations(bool creator, float damageValue, float speedValue, float rangeValue) {
         fromPlayer = creator;
@@ -15,13 +17,17 @@ public class Tir : MonoBehaviour {
     }
 
     private void Update() {
-        float distance = speed * Time.deltaTime;
+        float distance = speed / ralentissement * Time.deltaTime;
         transform.Translate(Vector3.up * distance);
         distanceTraveled += distance;
 
         if (distanceTraveled >= range) {
             Destroy(gameObject);
         }
+    }
+    
+    public bool IsFromPlayer() {
+        return fromPlayer;
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -30,5 +36,9 @@ public class Tir : MonoBehaviour {
             hit.TakeDamage(damage);
             Destroy(gameObject);
         }
+    }
+    
+    public void InSlowArea(float power) {
+        ralentissement += power;
     }
 }
