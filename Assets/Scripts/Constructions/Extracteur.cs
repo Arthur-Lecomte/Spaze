55d747d60b98307fda2ae9a2d48b0ef4dc009399
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-03-26 20:42:28 +0100

    {Fix} Extracteur Animation et Recolte syncronisé
    Il pouvait arrivé que l'animation de laser se joue sans que le joueur ne récolte de l'argent, modification du programme pour éviter cela (il peut quand même y avoir un très léger temps de latence entre les deux).

--- Assets/Scripts/Constructions/Extracteur.cs
+++ Assets/Scripts/Constructions/Extracteur.cs
@@ -4,31 +4,21 @@ public class Extracteur : SearchTag {
     [SerializeField] private float quantity;
     [SerializeField] private float quantityWithPercent;
     private static float percent;
-    
+
     private LaserBeamExtractor laserBeamExtractor;
-    
+
     private Animator animator;
     private static readonly int Extract = Animator.StringToHash("Extract");
-    
+
     public override void Initialisation(RarityConstruction rarityConstruction) {
         base.Initialisation(rarityConstruction);
         turnTransform = transform.GetChild(0);
-        
+
         animator = GetComponentInChildren<Animator>();
         laserBeamExtractor = GetComponentInChildren<LaserBeamExtractor>();
         OnPercentChanged(TypeUpgrade.Extraction, percent);
     }
     
-    protected override void DoAction(Transform target) {
-        animator.SetBool(Extract, true);
-        laserBeamExtractor.EnableLaser();
-        if (!target.gameObject.GetComponent<Structure>().Extract(quantityWithPercent)) {
-            InRange.Remove(target.gameObject.GetComponent<Collider>());
-            StopAction();
-        }
-    }
-
-
     public new void Update() {
         base.Update();
 
@@ -38,29 +28,39 @@ public class Extracteur : SearchTag {
         }
     }
 
+    protected override void DoAction(Transform target) {
+        if (!target.gameObject.GetComponent<Structure>().Extract(quantityWithPercent)) {
+            InRange.Remove(target.gameObject.GetComponent<Collider>());
+            StopAnimation();
+        }
+    }
+    
+    protected override void DoAnimation(Transform target) {
+        animator.SetBool(Extract, true);
+        laserBeamExtractor.EnableLaser();
+    }
 
-    protected override void StopAction()
-    {
+    protected override void StopAnimation() {
         animator.SetBool(Extract, false);
         laserBeamExtractor.DisableLaser();
     }
-    
+
     protected override void PerformUpgrade() {
         OnPercentChanged(TypeUpgrade.Extraction, percent);
     }
-    
+
     protected override void OnPercentChanged(TypeUpgrade typeUpgrade, float value) {
         if (value == 0) {
             UpgradeColumn.getValue(typeUpgrade);
             return;
         }
-        
+
         if (typeUpgrade == TypeUpgrade.Extraction) {
             percent = value;
             quantityWithPercent = quantity * percent;
         }
     }
-    
+
     public void OnDrawGizmos() {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, range);
