0292f0d08f01fc3dffde058731e085287e4a1bc8
Arthur Lecomte <arthur.lecomte.pro83@gmail.com>
2025-03-27 21:10:04 +0100

    {Update} taille et ressource astéroide

--- Assets/Scripts/Ressources/GenerationStructure.cs
+++ Assets/Scripts/Ressources/GenerationStructure.cs
@@ -120,7 +120,7 @@ public class GenerationStructure : MonoBehaviour {
                 obj = TryInstantiateVariant(asteroidData.variants, cellCenter, asteroidParent, cellSeed);
             else if (wreckChance < 0.34f)  // 10% de chance pour une épave
                 obj = TryInstantiateVariant(wreckData.variants, cellCenter, wreckParent, cellSeed);
-            else if (shopChance < 0.26f)  // 2% de chance pour un magasin
+            else if (shopChance < 0.05f)  // 2% de chance pour un magasin
                 obj = TryInstantiateObject(shopPrefab, cellCenter, shopParent, cellSeed);
 
 
@@ -158,9 +158,9 @@ public class GenerationStructure : MonoBehaviour {
         // Assigner une ressource à la structure si applicable
         if (obj.TryGetComponent<Structure>(out var structure)) {
             if (structure.isAsteroid) {
-                ChoseQuantityAsteroid(structure, ressourceType, cellSeed);
+                ChooseQuantityAsteroid(structure, ressourceType, cellSeed);
             } else {
-                ChoseQuantityEpave(structure, ressourceType, cellSeed);
+                ChooseQuantityEpave(structure, ressourceType, cellSeed);
             }
         }
         return obj;
@@ -189,46 +189,23 @@ public class GenerationStructure : MonoBehaviour {
         }
     }
 
-    private void ChoseQuantityAsteroid(Structure structure, TypeRessource ressourceType, int cellSeed) {
+    private void ChooseQuantityAsteroid(Structure structure, TypeRessource ressourceType, int cellSeed) {
         System.Random random = new(cellSeed);
-        int randomValue = random.Next(0, 101);
         Transform structureTransform = structure.transform;
-        int returnRessourceValue;
-        switch (ressourceType) {
-            case TypeRessource.Cuivre:
-                returnRessourceValue = randomValue <= 90 ? random.Next(0, 51) : 0;
-                structureTransform.localScale = new Vector3(1, 1, 1) * Mathf.Max(0.4f, returnRessourceValue / 25.0f);
-                break;
-            case TypeRessource.Argent:
-                returnRessourceValue = randomValue <= 80 ? random.Next(0, 51) : 0;
-                structureTransform.localScale = new Vector3(1, 1, 1) * Mathf.Max(0.6f, returnRessourceValue / 25.0f);
-                break;
-            case TypeRessource.Or:
-                returnRessourceValue = randomValue <= 60 ? random.Next(0, 26) : 0;
-                structureTransform.localScale = new Vector3(1, 1, 1) * Mathf.Max(0.7f, returnRessourceValue / 12.5f);
-                break;
-            case TypeRessource.Platine:
-                returnRessourceValue = randomValue <= 40 ? random.Next(0, 11) : 0;
-                structureTransform.localScale = new Vector3(1, 1, 1) * Mathf.Max(0.8f, returnRessourceValue / 5.0f);
-                break;
-            default:
-                returnRessourceValue = 0;
-                structureTransform.localScale = new Vector3(1, 1, 1) * random.Next(1, 2);
-                break;
-        }
+        int returnRessourceValue = random.Next(0, 100);
+        structureTransform.localScale = new Vector3(1,1,1) * (1.5f + returnRessourceValue / 50f);
 
         structure.SetRessource(new Ressource(ressourceType, returnRessourceValue));
     }
 
-    private void ChoseQuantityEpave(Structure structure, TypeRessource ressourceType, int cellSeed) {
+    private void ChooseQuantityEpave(Structure structure, TypeRessource ressourceType, int cellSeed) {
         System.Random random = new(cellSeed);
-        int randomValue = random.Next(0, 101);
         var returnRessourceValue = ressourceType switch {
-            TypeRessource.Cuivre => randomValue <= 90 ? random.Next(0, 51) : 0,
-            TypeRessource.Argent => randomValue <= 80 ? random.Next(0, 51) : 0,
-            TypeRessource.Or => randomValue <= 60 ? random.Next(0, 26) : 0,
-            TypeRessource.Platine => randomValue <= 40 ? random.Next(0, 11) : 0,
-            TypeRessource.NoyauEnergie => randomValue <= 3 ? 2 : 1,
+            TypeRessource.Cuivre => random.Next(0, 51),
+            TypeRessource.Argent => random.Next(0, 51),
+            TypeRessource.Or => random.Next(0, 26),
+            TypeRessource.Platine => random.Next(0, 11),
+            TypeRessource.NoyauEnergie => random.Next(1, 2),
             _ => 0,
         };
         structure.ressource = new Ressource(ressourceType, returnRessourceValue);
