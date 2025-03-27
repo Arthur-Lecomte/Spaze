375a750fd8d47855144ab900a0ee72a97ab3b7d7
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-03-27 17:36:54 +0100

    {Fix} Oublie d'initialiser les Constructions Speed

--- Assets/Scripts/Player/Vaisseau.cs
+++ Assets/Scripts/Player/Vaisseau.cs
@@ -73,6 +73,7 @@ public class Vaisseau : MonoBehaviour, ICanTakeDamage {
 
         // Appliquer une légère friction pour l'inertie
         rb.linearVelocity *= drag;
+        Debug.Log(rb.linearVelocity);
     }
 
     public void TakeDamage(float damage) {
